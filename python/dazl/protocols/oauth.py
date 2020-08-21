703bb0db2975cb0054782517ceec225a03d07983
nycnewman <edward@digitalasset.com>
2020-08-20 23:28:44 -0400

    Implement lookup based on client id and secret

--- python/dazl/protocols/oauth.py
+++ python/dazl/protocols/oauth.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from ..model.network import OAuthSettings
+import requests
 
 
 async def oauth_flow(settings: OAuthSettings) -> OAuthSettings:
@@ -12,11 +13,36 @@ async def oauth_flow(settings: OAuthSettings) -> OAuthSettings:
     :return:
     """
     if not settings.token:
-        raise ValueError('the token must be directly supplied at this time')
-        # if settings.redirect_uri:
-        #     client = WebApplicationClient(settings.client_id)
-        #     client.prepare_request_uri()
-        #     client.prepare_request_body()
-        #     client.parse_request_uri_response()
-        #     settings.
-    return settings
+        headers = { 'Accept': 'application/json' }
+        data = {
+            'client_id': settings.client_id,
+            'client_secret': settings.client_secret,
+            'audience': "https://daml.com/ledger-api",
+            'grant_type': "client_credentials"
+        }
+
+        response = None
+        try:
+            response = requests.post(settings.token_uri, headers=headers, data=data, auth=None)
+        except Exception as ex:
+            print(ex)
+            raise ValueError('the token must be directly supplied at this time')
+
+        if response.status_code != 200:
+            logging.error("ERROR: Unable to retrieve token. Exiting")
+            raise ValueError('Unable to get token frpm oAuth source')
+
+        json = response.json()
+        return_settings = OAuthSettings(
+            client_id=settings.client_id,
+            client_secret=settings.client_secret,
+            token=json['access_token'],
+            token_uri=settings.token_uri,
+            refresh_token=settings.refresh_token,
+            id_token=settings.id_token,
+            auth_url=settings.auth_url,
+            redirect_uri=settings.redirect_uri
+        )
+
+    else
+        return settings
