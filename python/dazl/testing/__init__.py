65315ada2948803088b081373872f1afe8700d38
Davin K. Tanabe <davin.tanabe@digitalasset.com>
2021-05-24 10:45:29 -0400

    python: Add some test utilities.

---
+++ python/dazl/testing/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2017-2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from .connect import connect_with_new_party
+
+__all__ = ["connect_with_new_party"]
--- python/tests/unit/test_protocol_ledgerapi.py
+++ python/tests/unit/test_protocol_ledgerapi.py
@@ -3,7 +3,7 @@
 
 import logging
 
-from dazl import connect
+from dazl.testing import connect_with_new_party
 import pytest
 
 from .dars import PostOffice
@@ -12,19 +12,13 @@ from .dars import PostOffice
 @pytest.mark.asyncio
 async def test_protocol_ledger_api(sandbox):
     # first, administrative stuff--upload the DAR and allocate two parties that we'll use later
-    async with connect(url=sandbox, admin=True) as conn:
-        await conn.upload_package(PostOffice.read_bytes())
-        postman = (await conn.allocate_party()).party
-        participant = (await conn.allocate_party()).party
-
-    async with connect(url=sandbox, act_as=postman) as conn:
-        event = await conn.create("Main:PostmanRole", {"postman": postman})
-        result = await conn.exercise(
-            event.contract_id, "InviteParticipant", {"party": participant, "address": "Somewhere!"}
+    async with connect_with_new_party(url=sandbox, dar=PostOffice, party_count=2) as (postman, p1):
+        event = await postman.connection.create("Main:PostmanRole", {"postman": postman.party})
+        result = await postman.connection.exercise(
+            event.contract_id, "InviteParticipant", {"party": p1.party, "address": "Somewhere!"}
         )
         logging.info("Result of inviting a participant: %s", result)
 
-    async with connect(url=sandbox, act_as=participant) as conn:
         # Stream results for Main:InviteAuthorRole, and then Main:InviteReceiverRole. Then break the
         # stream once we find the first contract.
         #
@@ -32,15 +26,15 @@ async def test_protocol_ledger_api(sandbox):
         # postman inviting participants may not yet have been observed by the clients. Instead, use
         # stream() since it remains open until explicitly closed. We break the never-ending iterator
         # as soon as we see one of each contract.
-        async with conn.stream("Main:InviteAuthorRole") as query:
+        async with p1.connection.stream("Main:InviteAuthorRole") as query:
             async for event in query:
-                result = await conn.exercise(event.contract_id, "AcceptInviteAuthorRole")
+                result = await p1.connection.exercise(event.contract_id, "AcceptInviteAuthorRole")
                 logging.info("The result of AcceptInviteAuthorRole: %s", result)
                 break
 
-        async with conn.stream("Main:InviteReceiverRole") as query:
+        async with p1.connection.stream("Main:InviteReceiverRole") as query:
             async for event in query:
-                result = await conn.exercise(event.contract_id, "AcceptInviteReceiverRole")
+                result = await p1.connection.exercise(event.contract_id, "AcceptInviteReceiverRole")
                 logging.info("The result of AcceptInviteReceiverRole: %s", result)
                 break
 
