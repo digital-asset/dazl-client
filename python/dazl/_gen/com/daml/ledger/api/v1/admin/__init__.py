7f1f80f926704288e8f6752cf025bce793fe5270
Davin K. Tanabe <davin.tanabe@digitalasset.com>
2021-08-20 00:06:23 -0400

    python: More improvements on code generated from Protobuf files.

--- python/dazl/_gen/com/daml/ledger/api/v1/admin/__init__.py
+++ python/dazl/_gen/com/daml/ledger/api/v1/admin/__init__.py
@@ -2,3 +2,40 @@
 # SPDX-License-Identifier: Apache-2.0
 # fmt: off
 # isort: skip_file
+
+from .config_management_service_pb2 import GetTimeModelRequest, GetTimeModelResponse, SetTimeModelRequest, SetTimeModelResponse, TimeModel
+from .config_management_service_pb2_grpc import ConfigManagementServiceStub
+from .package_management_service_pb2 import ListKnownPackagesRequest, ListKnownPackagesResponse, PackageDetails, UploadDarFileRequest, UploadDarFileResponse
+from .package_management_service_pb2_grpc import PackageManagementServiceStub
+from .participant_pruning_service_pb2 import PruneRequest, PruneResponse
+from .participant_pruning_service_pb2_grpc import ParticipantPruningServiceStub
+from .party_management_service_pb2 import AllocatePartyRequest, AllocatePartyResponse, GetParticipantIdRequest, GetParticipantIdResponse, GetPartiesRequest, GetPartiesResponse, ListKnownPartiesRequest, ListKnownPartiesResponse, PartyDetails
+from .party_management_service_pb2_grpc import PartyManagementServiceStub
+
+__all__ = [
+    "AllocatePartyRequest",
+    "AllocatePartyResponse",
+    "ConfigManagementServiceStub",
+    "GetParticipantIdRequest",
+    "GetParticipantIdResponse",
+    "GetPartiesRequest",
+    "GetPartiesResponse",
+    "GetTimeModelRequest",
+    "GetTimeModelResponse",
+    "ListKnownPackagesRequest",
+    "ListKnownPackagesResponse",
+    "ListKnownPartiesRequest",
+    "ListKnownPartiesResponse",
+    "PackageDetails",
+    "PackageManagementServiceStub",
+    "ParticipantPruningServiceStub",
+    "PartyDetails",
+    "PartyManagementServiceStub",
+    "PruneRequest",
+    "PruneResponse",
+    "SetTimeModelRequest",
+    "SetTimeModelResponse",
+    "TimeModel",
+    "UploadDarFileRequest",
+    "UploadDarFileResponse",
+]
--- python/dazl/_gen/com/daml/ledger/api/v1/testing/__init__.py
+++ python/dazl/_gen/com/daml/ledger/api/v1/testing/__init__.py
@@ -2,3 +2,17 @@
 # SPDX-License-Identifier: Apache-2.0
 # fmt: off
 # isort: skip_file
+
+from .reset_service_pb2 import ResetRequest
+from .reset_service_pb2_grpc import ResetServiceStub
+from .time_service_pb2 import GetTimeRequest, GetTimeResponse, SetTimeRequest
+from .time_service_pb2_grpc import TimeServiceStub
+
+__all__ = [
+    "GetTimeRequest",
+    "GetTimeResponse",
+    "ResetRequest",
+    "ResetServiceStub",
+    "SetTimeRequest",
+    "TimeServiceStub",
+]
