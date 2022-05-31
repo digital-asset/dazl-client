5acd44e7904d882a4b829b0d54d8cda8f775a782
Davin K. Tanabe <davin.tanabe@digitalasset.com>
2022-05-31 11:44:09 -0400

    python: Miscellaneous fixes to improve compatibility with Daml 2 ledgers running behind auth.

---
+++ python/dazl/ledger/_retry.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2017-2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+from asyncio import sleep
+from datetime import datetime, timedelta
+from typing import Awaitable, Callable, TypeVar
+
+from grpc import RpcError, StatusCode
+
+T = TypeVar("T")
+
+DEFAULT_TIMEOUT = timedelta(minutes=1)
+
+
+def is_retryable_exception(ex: Exception) -> bool:
+    if not isinstance(ex, RpcError):
+        return False
+
+    # TODO: Expand on these categories as documented here:
+    #  https://docs.daml.com/app-dev/grpc/error-codes.html
+    return ex.code() == StatusCode.FAILED_PRECONDITION and ex.details().startswith(
+        "PARTY_ALLOCATION_WITHOUT_CONNECTED_DOMAIN"
+    )
+
+
+async def retry(
+    fn: Callable[[], Awaitable[T]],
+    *,
+    retry_safe_ex: Callable[[Exception], bool] = is_retryable_exception,
+    timeout: timedelta = DEFAULT_TIMEOUT
+) -> T:
+    a, b = 0, 1
+    start = datetime.utcnow()
+    while True:
+        try:
+            return await fn()
+        except Exception as ex:
+            if (datetime.now() - start) <= timeout and retry_safe_ex(ex):
+                # retry in increasing intervals according to the Fibonacci sequence;
+                # exponential backoff generally makes us wait too long
+                a, b = b, a + b
+                await sleep(a)
+            else:
+                raise
--- python/tests/unit/test_ledger_token.py
+++ python/tests/unit/test_ledger_token.py
@@ -3,19 +3,45 @@
 from asyncio import sleep
 
 from dazl import connect, testing
+from dazl.ledger import ActAs, Admin, User
 from dazl.ledger.config.access import DamlLedgerApiNamespace
 import pytest
 
-from .config import daml_sdk_versions
+from .config import daml_sdk_versions, known_version_2
+from .dars import PostOffice
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("daml_sdk_version", daml_sdk_versions())
 async def test_v1_token(daml_sdk_version):
-    with testing.sandbox(version=daml_sdk_version, use_auth=True, ledger_id="sandbox") as sandbox:
+    async with testing.sandbox(
+        version=daml_sdk_version, use_auth=True, ledger_id="sandbox"
+    ) as sandbox:
         token = sandbox.sign_token({DamlLedgerApiNamespace: {"admin": True, "ledgerId": "sandbox"}})
         async with connect(url=sandbox.url, oauth_token=token) as conn:
             # the result of this call is not particularly interesting;
             # we just need to make sure it doesn't crash
             await conn.list_package_ids()
             await sleep(1)
+
+
+@pytest.mark.asyncio
+async def test_v2_token(sandbox_v2):
+    async with testing.sandbox(
+        version=known_version_2, use_auth=True, ledger_id="sandbox"
+    ) as sandbox:
+        # use an anonymous admin Daml V1 token to bootstrap users, because that's unfortunately
+        # the only way
+        token = sandbox.sign_token({DamlLedgerApiNamespace: {"admin": True, "ledgerId": "sandbox"}})
+        async with connect(url=sandbox.url, oauth_token=token) as conn:
+            p = await conn.allocate_party(identifier_hint="alice", display_name="alice")
+            await conn.create_user(User("alice", p.party), [Admin, ActAs(p.party)])
+            await conn.upload_package(PostOffice.read_bytes())
+
+        token = sandbox.sign_token({"sub": "alice", "scope": "daml_ledger_api"})
+        async with connect(url=sandbox.url, oauth_token=token) as conn:
+            # make sure we have admin rights
+            await conn.allocate_party()
+
+            # make sure we can create contracts
+            await conn.create("Main:PostmanRole", {"postman": p.party})
