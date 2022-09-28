a4b9e3db96f1067b1823aa21acc48319c6dccf52
Davin K. Tanabe <davin.tanabe@digitalasset.com>
2022-09-28 13:44:54 -0400

    python: Avoid using pkg_resources.

--- python/dazl/__init__.py
+++ python/dazl/__init__.py
@@ -4,11 +4,6 @@
 """
 This module contains the Python API for interacting with the Ledger API.
 """
-from ast import literal_eval
-from configparser import ConfigParser
-from pathlib import Path
-
-import pkg_resources
 
 __all__ = [
     "AIOPartyClient",
@@ -70,33 +65,4 @@ except ImportError:
     pass
 
 
-def _get_version() -> str:
-    """
-    Used to make the version of this library easily accessible programmatically.
-    Two techniques are tried:
-     1. Try to read it from the current package definition. This is what is used
-        when trying to look up version information if dazl is installed via a
-        wheel file.
-     2. Use the value from the local pyproject.toml file (this is used when
-        running dazl from source).
-    """
-    try:
-        return pkg_resources.require("dazl")[0].version
-    except pkg_resources.DistributionNotFound:
-        pass
-    except Exception:
-        return "unknown"
-
-    try:
-        config = ConfigParser()
-        config.read(Path(__file__).parent.parent / "pyproject.toml")
-        if "tool.poetry" in config:
-            poetry_section = config["tool.poetry"]
-            return literal_eval(poetry_section["version"])
-    except Exception:  # noqa
-        pass
-
-    return "unknown"
-
-
-__version__ = _get_version()
+__version__ = "7.8.4"
