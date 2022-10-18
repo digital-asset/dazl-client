615a315369e43a5d6c50e5dd676deea4935b189a
Davin K. Tanabe <davin.tanabe@digitalasset.com>
2022-10-18 12:31:44 -0400

    python: Add a test case for CreateAndExerciseCommand.

---
+++ python/tests/unit/test_ledger_create_and_exercise.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2017-2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from dazl import connect
+from dazl.testing import SandboxLauncher
+import pytest
+
+from .dars import PostOffice
+
+
+@pytest.mark.asyncio
+async def test_ledger_create_and_exercise(sandbox_v2: SandboxLauncher) -> None:
+    async with connect(url=sandbox_v2.url, admin=True) as conn:
+        await conn.upload_package(PostOffice.read_bytes())
+
+        party_info = await conn.allocate_party()
+        operator = party_info.party
+        await conn.create_and_exercise(
+            "Main:PostmanRole",
+            {"postman": operator},
+            "InviteParticipant",
+            {"party": operator, "address": "21 Jump Street"},
+            act_as=operator,
+        )
