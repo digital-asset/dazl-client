511b22f9d6fd87d8c766695e37d8b6250936b21b
Davin K. Tanabe <davin.tanabe@digitalasset.com>
2022-12-24 18:10:45 -0500

    python: Drop most of dazl.pretty.

--- python/dazl/pretty/__init__.py
+++ python/dazl/pretty/__init__.py
@@ -7,27 +7,19 @@
 
 This module contains utilities for pretty-printing various types in dazl.
 
-.. automodule:: dazl.pretty.render_daml
-.. automodule:: dazl.pretty.util
 """
 
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Optional, Type
 
-from ..damlast.protocols import SymbolLookup
-from ._render_base import PrettyPrintBase, pretty_print_syntax  # type: ignore
-from .options import PrettyOptions
-from .render_csharp import CSharpPrettyPrint  # type: ignore
-from .render_daml import DEFAULT_PRINTER as DAML_PRETTY_PRINTER, DamlPrettyPrinter  # type: ignore
-from .render_python import PythonPrettyPrint  # type: ignore
 from .util import maybe_parentheses
 
 if TYPE_CHECKING:
     from .pygments_daml_lexer import DAMLLexer as _DAMLLexer_TYPE
 
 
-__all__ = ["get_pretty_printer", "DAMLLexer", "ALL_PRINTER_TYPES", "PrettyOptions"]
+__all__ = ["DAMLLexer"]
 
 
 def _import_daml_lexer() -> "Optional[Type[_DAMLLexer_TYPE]]":
@@ -41,16 +33,3 @@ def _import_daml_lexer() -> "Optional[Type[_DAMLLexer_TYPE]]":
 
 
 DAMLLexer = _import_daml_lexer()
-
-
-ALL_PRINTER_TYPES = [CSharpPrettyPrint, DamlPrettyPrinter, PythonPrettyPrint]  # type: ignore
-
-
-# noinspection PyShadowingBuiltins,PyShadowingNames
-def get_pretty_printer(
-    format: str, options: "PrettyOptions", lookup: "SymbolLookup"
-) -> "Optional[PrettyPrintBase]":
-    for printer in ALL_PRINTER_TYPES:
-        if printer.syntax.startswith(format):
-            return printer(lookup, options)
-    return None
