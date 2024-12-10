8665b8453ff60df69bddabf918c7e301e1a0b766
Mike Schaeffer <mike.schaeffer@digitalasset.com>
2024-12-10 11:07:38 -0500

    Allow user based token auth against default sandboxes.

--- python/tests/unit/test_ledger_token.py
+++ python/tests/unit/test_ledger_token.py
@@ -28,7 +28,7 @@ async def test_v1_token(daml_sdk_version: str) -> None:
 
 
 @pytest.mark.asyncio
-async def test_v2_token() -> None:
+async def test_v2_token_auth_sandbox() -> None:
     async with testing.sandbox(
         version=known_version_2_9, use_auth=True, ledger_id="sandbox"
     ) as sandbox:
@@ -41,6 +41,49 @@ async def test_v2_token() -> None:
             await conn.upload_package(PostOffice.read_bytes())
 
         token = sandbox.sign_token({"sub": "alice", "scope": "daml_ledger_api"})
+        async with connect(url=sandbox.url, oauth_token=token) as conn:
+            # make sure we have admin rights
+            await conn.allocate_party()
+
+            # make sure we can create contracts
+            await conn.create("Main:PostmanRole", {"postman": p.party})
+
+
+@pytest.mark.asyncio
+async def test_v2_no_auth_sandbox_cannot_sign_token_by_default() -> None:
+    async with testing.sandbox(version=known_version_2_9, ledger_id="sandbox") as sandbox:
+        # use an anonymous admin Daml V1 token to bootstrap users, because that's unfortunately
+        # the only way
+        failure_message = None
+        try:
+            token = sandbox.sign_token(
+                {DamlLedgerApiNamespace: {"admin": True, "ledgerId": "sandbox"}}
+            )
+        except RuntimeError as ex:
+            # Expected error when allow_insecure is not True and the
+            # underlying ledger is not running with use_auth.
+            failure_message = str(ex)
+
+        print(f">>>> {failure_message}")
+        assert failure_message == "this sandbox was not started with auth"
+
+
+@pytest.mark.asyncio
+async def test_v2_token_no_auth_sandbox() -> None:
+    async with testing.sandbox(version=known_version_2_9, ledger_id="sandbox") as sandbox:
+        # use an anonymous admin Daml V1 token to bootstrap users, because that's unfortunately
+        # the only way
+        token = sandbox.sign_token(
+            {DamlLedgerApiNamespace: {"admin": True, "ledgerId": "sandbox"}}, allow_insecure=True
+        )
+        async with connect(url=sandbox.url, oauth_token=token) as conn:
+            p = await conn.allocate_party(identifier_hint="alice", display_name="alice")
+            await conn.create_user(User("alice", p.party), [Admin, ActAs(p.party)])
+            await conn.upload_package(PostOffice.read_bytes())
+
+        token = sandbox.sign_token(
+            {"sub": "alice", "scope": "daml_ledger_api"}, allow_insecure=True
+        )
         async with connect(url=sandbox.url, oauth_token=token) as conn:
             # make sure we have admin rights
             await conn.allocate_party()
