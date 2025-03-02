779109a22dadbb8fa0aa7d1995bae51fa220bf8c
Davin K. Tanabe <davin.tanabe@digitalasset.com>
2025-03-02 14:39:25 -0500

    python: Rename the two KitchenSink test DARs so that they don't conflict with each other.

--- python/tests/unit/test_historical_dar_parsing.py
+++ python/tests/unit/test_historical_dar_parsing.py
@@ -11,7 +11,7 @@ from dazl.damlast import DarFile
 from dazl.damlast.lookup import MultiPackageLookup
 import pytest
 
-from .dars import KitchenSink1_18, KitchenSink2_9
+from .dars import KitchenSink1, KitchenSink2
 
 ARCHIVES: Path = Path(__file__).absolute().parent.parent.parent.parent / "_fixtures" / "archives"
 
@@ -33,29 +33,29 @@ def test_dar_version_compatibility(dar):
     )
 
 
-def test_dar_kitchen_sink_1_18():
+def test_dar_kitchen_sink_1():
     start_time = time.time()
-    dar_file = DarFile(KitchenSink1_18)
+    dar_file = DarFile(KitchenSink1)
     archives = dar_file.archives()
     end_time = time.time()
     LOG.info(
-        "Successfully read KitchenSink (1.18) in %0.2f seconds with package IDs %r.",
+        "Successfully read Kitchen Sink 1 in %0.2f seconds with package IDs %r.",
         end_time - start_time,
         [a.hash for a in archives],
     )
 
 
-def test_dar_kitchen_sink_2_9() -> None:
+def test_dar_kitchen_sink_2() -> None:
     start_time = time.time()
-    dar_file = DarFile(KitchenSink2_9)
+    dar_file = DarFile(KitchenSink2)
     archives = dar_file.archives()
     end_time = time.time()
     LOG.info(
-        "Successfully read KitchenSink (2.9) in %0.2f seconds with package IDs %r.",
+        "Successfully read Kitchen Sink 2 in %0.2f seconds with package IDs %r.",
         end_time - start_time,
         [a.hash for a in archives],
     )
 
     lookup = MultiPackageLookup(dar_file.archives())
-    iface = lookup.interface("KitchenSink.Interfaces.HasLocation:HasLocation")
+    iface = lookup.interface("KitchenSink2.Interfaces.HasLocation:HasLocation")
     assert iface is not None
